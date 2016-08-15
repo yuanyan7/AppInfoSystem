30a5c9b569e8a3fc9e8b90eba8df9409e7a06a91
findlyhl <findlyhl@gmail.com>
2016-08-15 22:50:07 +0800

    test002

---
+++ src/cn/appsys/service/developer/AppCategoryServiceImpl.java
@@ -0,0 +1,16 @@
+package cn.appsys.service.developer;
+
+import java.util.List;
+
+import cn.appsys.pojo.AppCategory;
+
+public class AppCategoryServiceImpl implements AppCategoryService {
+
+	@Override
+	public List<AppCategory> getAppCategoryList(Integer parentId)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
